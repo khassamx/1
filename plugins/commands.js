b47b4b5ed058d1615f9f212108d46d76a3dcfc93
khassamx <khasamxx@gmail.com>
2025-10-06 02:33:38 -0300

    Actualizar commands.js

--- plugins/commands.js
+++ plugins/commands.js
@@ -4,7 +4,7 @@ console.log('✅ Plugin: commands.js cargado (Comandos Generales)');
 
 export function handleGeneralCommands(conn, m, args) {
     if (m.text === '!menu') {
-        conn.sendMessage(m.chat, { text: 'Menú de Vegeta-Bot-MB: !ig, !tiktok, !play.' }, { quoted: m });
+        conn.sendMessage(m.chat, { text: 'Menú de Vegeta-Bot-MB: !ig, !tiktok, !kick, !play.' }, { quoted: m });
     }
     // Añadir más comandos generales aquí (e.g., !status, !help)
 }

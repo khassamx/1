cd909720ccace9c41ff55d94259aff4f97c80ea2
khassamx <khasamxx@gmail.com>
2025-10-06 02:59:36 -0300

    Actualizar main-menu.js

--- plugins/main-menu.js
+++ plugins/main-menu.js
@@ -1,50 +1,58 @@
-// ⌬ Menú Eʟɪᴛᴇ Minimalista con Contador Global
+// ⌬ Menú MALLY BOT con letra estilizada segura
 // Creado por Khassam | Developer: Brayan OFC
 
 import fetch from 'node-fetch'
 
-const botname = '⌬ MALLY BOT'
+const botname = '⌬ MALLY Ϟ BOT ⌬'
 const creador = 'KHASSAM'
 const developer = 'BRAYAN OFC'
 
+// Función solo para títulos y secciones
+function fancyText(text) {
+  const map = {
+    'a':'ᴀ','b':'ʙ','c':'ᴄ','d':'ᴅ','e':'ᴇ','f':'ꜰ','g':'ɢ','h':'ʜ','i':'ɪ','j':'ᴊ',
+    'k':'ᴋ','l':'ʟ','m':'ᴍ','n':'ɴ','o':'ᴏ','p':'ᴘ','q':'ǫ','r':'ʀ','s':'ꜱ','t':'ᴛ',
+    'u':'ᴜ','v':'ᴠ','w':'ᴡ','x':'x','y':'ʏ','z':'ᴢ','A':'A','B':'B','C':'C','D':'D','E':'E',
+    'F':'F','G':'G','H':'H','I':'I','J':'J','K':'K','L':'L','M':'M','N':'N','O':'O','P':'P',
+    'Q':'Q','R':'R','S':'S','T':'T','U':'U','V':'V','W':'W','X':'X','Y':'Y','Z':'Z'
+  }
+  return text.split('').map(c => map[c] || c).join('')
+}
+
 let handler = async (m, { conn, usedPrefix: _p }) => {
   try {
-    // Inicializar base de datos global si no existe
     if (!global.db) global.db = {}
     if (!global.db.data) global.db.data = {}
     if (!global.db.data.global) global.db.data.global = { totalMessages: 0 }
 
-    // Incrementar contador global de mensajes
     global.db.data.global.totalMessages += 1
     let totalGlobal = global.db.data.global.totalMessages
-
     let userId = m.mentionedJid?.[0] || m.sender
 
     let menuText = `
 ${botname}
 
-⌬ Creador: ${creador}
-⌬ Developer: ${developer}
-⌬ Mensajes globales: ${totalGlobal}
+⌬ ${fancyText('Creador')}: ${creador}
+⌬ ${fancyText('Developer')}: ${developer}
+⌬ ${fancyText('Mensajes globales')}: ${totalGlobal}
 
-⌬ GRUPOS
+⌬ ${fancyText('GRUPOS')}
 ⌬ .kick
 ⌬ .antilink
 
-⌬ DESCARGAS
+⌬ ${fancyText('DESCARGAS')}
 ⌬ .play
 ⌬ .tiktok
 ⌬ .ig
 
-⌬ CREADOR
+⌬ ${fancyText('CREADOR')}
 ⌬ .owner
 
-⌬ SUBBOT
+⌬ ${fancyText('SUBBOT')}
 ⌬ .qr
 ⌬ .code
 `
 
-    // Enviar menú
     await conn.sendMessage(m.chat, { text: menuText, contextInfo: { mentionedJid: [userId] } })
 
   } catch (e) {
@@ -52,8 +60,8 @@ ${botname}
   }
 }
 
-// Configuración del comando
+// Comandos que activan el menú
 handler.help = ['menu']
 handler.tags = ['main']
-handler.command = ['menu', 'mallymenu', 'elitemenu']
+handler.command = ['menu', 'mallymenu']
 export default handler
